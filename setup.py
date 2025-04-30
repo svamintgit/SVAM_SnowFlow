ed05cf904419c3d00de8754b78070023f0cbb0c0
Tommy Garcia <tgarcia@svam.com>
2025-04-30 11:11:20 -0400

    setup fix

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="snowflow",
-    version="0.0.14",
+    version="0.0.15",
     author="Thomas Garcia, Aryan Singh",
     author_email="tgarcia@svam.com, aryan.singh@svam.com",
     description="SnowFlow - A Snowflake Deployment Tool",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    packages=['snowflow'],
+    package_dir={'snowflow': 'snowflow'},
     include_package_data=True,
     package_data={'snowflow': ['*.sql', 'snowflow/*.sql']},
     install_requires=[
         "snowflake-connector-python",
         "snowflake-snowpark-python",
+        "snowflake-connector-python[secure-local-storage]",
         "networkx",
         "pyyaml",
         "toml",
